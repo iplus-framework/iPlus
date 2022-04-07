2b28f84e9986bdfd623be55240424ad403e60178
Damir Lisak <dlisak@kajbum.com>
2022-04-07 12:20:10 +0200

    Desicion Node with forced Jump

--- gip.core.autocomponent/ACProcessWorkflow/PWNode/PWNodeDecisionFunc.cs
+++ gip.core.autocomponent/ACProcessWorkflow/PWNode/PWNodeDecisionFunc.cs
@@ -43,13 +43,21 @@ namespace gip.core.autocomponent
 
         static PWNodeDecisionFunc()
         {
+            ACMethod method;
+            method = new ACMethod(ACStateConst.SMStarting);
+            Dictionary<string, string> paramTranslation = new Dictionary<string, string>();
+            method.ParameterValueList.Add(new ACValue("ForceEventPoint", typeof(ushort), 0, Global.ParamOption.Required));
+            paramTranslation.Add("ForceEventPoint", "en{'Raise Event [Off=0], [Below=1], [Sideward=2]'}de{'Erzwinge Ereignis [Aus=0], [Unten=1], [Seitlich/Else=2]'}");
+            var wrapper = new ACMethodWrapper(method, "en{'Configuration'}de{'Konfiguration'}", typeof(PWNodeDecisionFunc), paramTranslation, null);
+            ACMethod.RegisterVirtualMethod(typeof(PWNodeDecisionFunc), ACStateConst.SMStarting, wrapper);
+
+
             ACEventArgs TMP;
-
             _SVirtualEventArgs = new Dictionary<string,ACEventArgs>(PWBaseExecutable.SVirtualEventArgs, StringComparer.OrdinalIgnoreCase);
-
             TMP = new ACEventArgs();
             TMP.Add(new ACValue("TimeInfo", typeof(PATimeInfo), null, Global.ParamOption.Required));
             _SVirtualEventArgs.Add("PWPointElseOut", TMP);
+
             RegisterExecuteHandler(typeof(PWNodeDecisionFunc), HandleExecuteACMethod_PWNodeDecisionFunc);
         }
 
@@ -88,9 +96,43 @@ namespace gip.core.autocomponent
 
         #endregion
 
+        #region Properties
+        protected ushort ForceEventPoint
+        {
+            get
+            {
+                var method = MyConfiguration;
+                if (method != null)
+                {
+                    var acValue = method.ParameterValueList.GetACValue("ForceEventPoint");
+                    if (acValue != null)
+                    {
+                        return acValue.ParamAsUInt16;
+                    }
+                }
+                return 0;
+            }
+        }
+        #endregion
+
         #region Public
 
         #region Execute-Helper-Handlers
+        protected override bool HandleExecuteACMethod(out object result, AsyncMethodInvocationMode invocationMode, string acMethodName, ACClassMethod acClassMethod, params object[] acParameter)
+        {
+            result = null;
+            switch (acMethodName)
+            {
+                case nameof(RaiseElseEventAndComplete):
+                    RaiseElseEventAndComplete();
+                    return true;
+                //case Const.IsEnabledPrefix + "RaiseOutEvent":
+                //    result = IsEnabledRaiseElseEventAndComplete();
+                //    return true;
+            }
+            return base.HandleExecuteACMethod(out result, invocationMode, acMethodName, acClassMethod, acParameter);
+        }
+
         public static bool HandleExecuteACMethod_PWNodeDecisionFunc(out object result, IACComponent acComponent, string acMethodName, gip.core.datamodel.ACClassMethod acClassMethod, params object[] acParameter)
         {
             return HandleExecuteACMethod_PWBaseExecutable(out result, acComponent, acMethodName, acClassMethod, acParameter);
@@ -112,6 +154,7 @@ namespace gip.core.autocomponent
             base.SMIdle();
         }
 
+        [ACMethodState("en{'Executing'}de{'Ausführend'}", 20, true)]
         public override void SMStarting()
         {
             if (ParentPWGroup != null)
@@ -119,6 +162,15 @@ namespace gip.core.autocomponent
                 var processModule = ParentPWGroup.AccessedProcessModule;
                 if (processModule != null)
                     processModule.RefreshPWNodeInfo();
+            }
+
+            if (ForceEventPoint == 1)
+            {
+                RaiseOutEventAndComplete();
+            }
+            else if (ForceEventPoint == 2)
+            {
+                RaiseElseEventAndComplete();
             }
             base.SMStarting();
         }
@@ -128,8 +180,14 @@ namespace gip.core.autocomponent
             return TimeSpan.Zero;
         }
 
+        public override void RaiseOutEvent()
+        {
+            RaiseOutEventAndComplete();
+        }
+
         protected void RaiseOutEventAndComplete()
         {
+            ResetInEvents();
             RecalcTimeInfo();
             TimeInfo.ValueT.ActualTimes.ChangeTime(TimeInfo.ValueT.ActualTimes.StartTime == DateTime.MinValue ? DateTime.Now : TimeInfo.ValueT.ActualTimes.StartTime, DateTime.Now);
             ACEventArgs eventArgs = ACEventArgs.GetVirtualEventArgs("PWPointOut", VirtualEventArgs);
@@ -144,8 +202,10 @@ namespace gip.core.autocomponent
         }
 
 
-        protected void RaiseElseEventAndComplete()
+        [ACMethodInteraction("Process", "en{'Raise ELSE event'}de{'SONST-Ereignis auslösen'}", 2004, true, "", Global.ACKinds.MSMethod, false, Global.ContextMenuCategory.ProcessCommands)]
+        public void RaiseElseEventAndComplete()
         {
+            ResetInEvents();
             RecalcTimeInfo();
             TimeInfo.ValueT.ActualTimes.ChangeTime(TimeInfo.ValueT.ActualTimes.StartTime == DateTime.MinValue ? DateTime.Now : TimeInfo.ValueT.ActualTimes.StartTime, DateTime.Now);
             ACEventArgs eventArgs = ACEventArgs.GetVirtualEventArgs("PWPointElseOut", VirtualEventArgs);
