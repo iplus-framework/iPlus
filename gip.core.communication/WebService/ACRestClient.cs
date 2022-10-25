85200b027bd49a9427f5c1667ccdac3fa101d8e6
aagincic <aleksandar.agincic@gmail.com>
2022-10-25 17:27:38 +0200

    Advantec backup

--- gip.core.communication/WebService/ACRestClient.cs
+++ gip.core.communication/WebService/ACRestClient.cs
@@ -931,6 +931,7 @@ namespace gip.core.communication
             try
             {
                 Task<WSResponse<string>> task = PatchAsync(content, uri);
+                task.Wait(); // check ending data
                 return task.Result;
             }
             catch (Exception ex)
