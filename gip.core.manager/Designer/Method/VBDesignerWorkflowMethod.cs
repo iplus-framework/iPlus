4f3940c49e3411ba296108bccce2ea00fe9c8c93
Damir Lisak <dlisak@kajbum.com>
2023-10-31 10:08:06 +0100

    Posting only Quants that belongs to Lot-Reservations.

--- gip.core.manager/Designer/Method/VBDesignerWorkflowMethod.cs
+++ gip.core.manager/Designer/Method/VBDesignerWorkflowMethod.cs
@@ -772,7 +772,8 @@ namespace gip.core.manager
                 if (!workflowClass.GetIncomingWFEdges(rootACClassMethod).Any() || !workflowClass.GetOutgoingWFEdges(rootACClassMethod).Any())
                     return false;
 
-                if (parentACClass != workflowClass.ParentACClass)
+                if (   (parentACClass.ACKind != Global.ACKinds.TPAProcessModuleGroup && parentACClass != workflowClass.ParentACClass)
+                    || (parentACClass.ACKind == Global.ACKinds.TPAProcessModuleGroup && parentACClass.ACClass1_ParentACClass != workflowClass.ParentACClass))
                     return false;
 
                 if (workflowClass.WFGroup == null || !(workflowClass.WFGroup is ACClassWF))
