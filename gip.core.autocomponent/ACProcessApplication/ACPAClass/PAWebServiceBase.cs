13ebc303cfc063880249f86295cd058ca86c3cea
aagincic <aleksandar.agincic@gmail.com>
2024-02-05 15:50:03 +0100

    iP-T-23-10-17-001 │ NKG Interface PAXMLImportMaterialNKG

--- gip.core.autocomponent/ACProcessApplication/ACPAClass/PAWebServiceBase.cs
+++ gip.core.autocomponent/ACProcessApplication/ACPAClass/PAWebServiceBase.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Dispatcher;
-using System.Text;
-using System.Text.RegularExpressions;
 using gip.core.datamodel;
 
 namespace gip.core.autocomponent
