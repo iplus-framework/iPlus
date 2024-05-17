7e535d87badaa91d44296ad289529b9560456e19
Ivan Hrastinski <ihrastinski@kajbum.com>
2024-05-17 14:13:10 +0200

    Converter

---
+++ gip.core.layoutengine/Converter/ConverterBoolToIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace gip.core.layoutengine
+{
+    public class ConverterBoolToIndex : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return 2;
+
+            return ((bool)value == true) ? 0 : 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            int valueInt = (int)value;
+
+            if (valueInt == 0)
+                return true;
+
+            if (valueInt == 1)
+                return false;
+
+            return null;
+        }
+    }
+}
