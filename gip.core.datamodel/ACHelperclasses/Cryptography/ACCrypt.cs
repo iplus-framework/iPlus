8b5931ab621c0bb83af31b0855502a788669796c
Ivan Hrastinski <ihrastinski@kajbum.com>
2024-07-05 12:28:55 +0200

    Mobile

--- gip.core.datamodel/ACHelperclasses/Cryptography/ACCrypt.cs
+++ gip.core.datamodel/ACHelperclasses/Cryptography/ACCrypt.cs
@@ -35,7 +35,7 @@ namespace gip.core.datamodel
         public static string GetMd5Hash(string input)
         {
             MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
-            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+            byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
             StringBuilder sBuilder = new StringBuilder();
             for (int i = 0; i < data.Length; i++)
             {
