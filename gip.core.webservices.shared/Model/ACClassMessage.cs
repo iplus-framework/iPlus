088cfb6a31d4d93d51830024f1aff3331ff39c1b
Ivan Hrastinski <iveksp@gmail.com>
2025-01-23 23:04:47 +0100

    Mobile OEE Reason

---
+++ gip.core.webservices.shared/Model/ACClassMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace gip.core.webservices
+{
+    [DataContract]
+    public class ACClassMessage
+    {
+        [DataMember(Name = "ID")]
+        public Guid ACClassMessageID
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "ACI")]
+        public string ACIdentifier
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "ACC")]
+        public string ACCaption
+        {
+            get;
+            set;
+        }
+    }
+}
