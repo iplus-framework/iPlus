562c6fbd3742a505da6177736c3f9823f4faa3f0
Damir Lisak <dlisak@kajbum.com>
2025-06-11 18:34:46 +0200

    Bugfix. The ability to read config parameters remotely led to a side effect: proxy components attempted to subscribe to this address without the user's permissions. This resulted in an invalid serialization of a component in the service channel.

--- gip.core.reporthandler/VBBSOReportDialog.cs
+++ gip.core.reporthandler/VBBSOReportDialog.cs
@@ -1387,7 +1387,7 @@ namespace gip.core.reporthandler
         protected virtual VBBSOReport StartVBBSOReport()
         {
             string acClassName = "VBBSOReport";
-            ACClass acClass = GetACClassFromACClassName(ref acClassName);
+            ACClass acClass = GetACClassFromACClassName(acClassName);
             if (acClass == null)
                 return null;
             return StartComponent(acClass, acClass, null, Global.ACStartTypes.Automatic) as VBBSOReport;
