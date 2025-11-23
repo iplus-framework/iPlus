0ebad58e1ab23227b1e0c4a131aece28e8f06cbc
Damir Lisak <damir.lisak@live.de>
2025-11-23 12:57:04 +0100

    Compilation of Avalonia with Visual Studio Code on Linux fixed and tested.

--- gip.core.layoutengine.avui/Controls/VBListBox/VBListBox.cs
+++ gip.core.layoutengine.avui/Controls/VBListBox/VBListBox.cs
@@ -670,7 +670,7 @@ namespace gip.core.layoutengine.avui
             OnDoubleTapped(e);
         }
 
-        protected override void OnDoubleTapped(TappedEventArgs e)
+        protected void OnDoubleTapped(TappedEventArgs e)
         {
             if (DblClick != null && DblClick != "")
             {
