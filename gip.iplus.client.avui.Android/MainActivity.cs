066d41068839eef8fd498d79b562180ad2dc9d23
Damir Lisak <dlisak@kajbum.com>
2026-01-25 18:07:26 +0100

    Debugging on Linux + Waydroid. See also Commit 2cd85ae in Linux-Setup-Guide with Example files launch.json & tasks.json.

--- gip.iplus.client.avui.Android/MainActivity.cs
+++ gip.iplus.client.avui.Android/MainActivity.cs
@@ -11,11 +11,12 @@ namespace gip.iplus.client.avui.Android;
     Icon = "@drawable/icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity
+public class MainActivity : AvaloniaMainActivity<App>
 {
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
         return base.CustomizeAppBuilder(builder)
             .WithInterFont();
     }
+
 }
