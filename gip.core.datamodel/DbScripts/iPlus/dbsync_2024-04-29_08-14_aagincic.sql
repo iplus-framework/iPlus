7ecaecd1b0247b09f68d64f104aa24c37707d0ea
Ivan Hrastinski <iveksp@gmail.com>
2026-02-20 13:08:10 +0100

    DbScripts cleanup

--- gip.core.datamodel/DbScripts/iPlus/dbsync_2024-04-29_08-14_aagincic.sql
+++
@@ -1,4 +0,0 @@
-alter table [dbo].[ACClassProperty] add IsCaptionCustomized bit null;
-GO
-update [dbo].[ACClassProperty] set IsCaptionCustomized = 0
-alter table [dbo].[ACClassProperty] alter column IsCaptionCustomized bit not null;
