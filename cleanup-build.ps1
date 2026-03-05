a60e3a1d37bd57479d261c40df0867458d403abb
Aleksandar Agincic <aleksandar.agincic@iplus-framework.com>
2026-03-05 15:50:05 +0100

    move ps1 to scripts

--- cleanup-build.ps1
+++
@@ -1,5 +0,0 @@
-clean-bin-obj-folders.ps1 "c:\Devel\iplus-github\iPlus\"
-clean-bin-obj-folders.ps1 "c:\Devel\iplus-github\iPlus\"
-dotnet clean .\iplus.slnx
-dotnet restore .\iplus.slnx
-dotnet build .\iplus.slnx
